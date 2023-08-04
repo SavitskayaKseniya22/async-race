d7ece272f5122b28a7a9fc549669456ab478dc56
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2023-08-04 17:56:11 +0300

    refactor: api calls

--- async-race/src/api.ts
+++ async-race/src/api.ts
@@ -1,192 +1,237 @@
+/* eslint-disable consistent-return */
 import { Car, Winner } from "./types";
 
 class ApiService {
   static link = "http://127.0.0.1:3000";
 
   static async getCarsNames() {
-    const response = await fetch(`./assets/jsons/carNames.json`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`./assets/jsons/carNames.json`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const cars = await response.json();
       return cars as { [x: string]: string[] };
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async getAllCars() {
-    const response = await fetch(`${ApiService.link}/garage`);
-    if (response.ok) {
-      const allCars = await response.json();
-      return allCars as Car[];
+    try {
+      const response = await fetch(`${ApiService.link}/garage`);
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
+      const cars = await response.json();
+      return cars as Car[];
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async createCar(data: Car = {}) {
-    const response = await fetch(`${ApiService.link}/garage`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
-      const car = (await response.json()) as Car;
-      return car;
+    try {
+      const response = await fetch(`${ApiService.link}/garage`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
+      const car = await response.json();
+      return car as Car;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async deleteCar(id: number) {
-    const response = await fetch(`${ApiService.link}/garage/${id}`, {
-      method: "DELETE",
-    });
-
-    if (response.ok) {
-      return response.json();
+    try {
+      const response = await fetch(`${ApiService.link}/garage/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
+      const car = await response.json();
+      return car;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async deleteWinner(id: number) {
-    const response = await fetch(`${ApiService.link}/winners/${id}`, {
-      method: "DELETE",
-    });
-
-    if (response.ok) {
-      return response.json();
+    try {
+      const response = await fetch(`${ApiService.link}/winners/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
+      const car = await response.json();
+      return car;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async changeDriveMode(id: number, status: "started" | "stopped" | "drive") {
     const response = await fetch(`${ApiService.link}/engine?id=${id}&status=${status}`, {
       method: "PATCH",
     });
-
-    if (response.ok) {
-      return response.json();
+    if (response.status === 500) {
+      throw new Error(`Car №${id} has been stopped suddenly. It's engine was broken down`);
+    } else if (!response.ok) {
+      throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+    return response.json();
   }
 
   static async getCar(id: number) {
-    const response = await fetch(`${ApiService.link}/garage/${id}`, {
-      method: "GET",
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`${ApiService.link}/garage/${id}`, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const car = await response.json();
       return car as Car;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async updateCar(id: number, data: Car = {}) {
-    const response = await fetch(`${ApiService.link}/garage/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`${ApiService.link}/garage/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const car = await response.json();
       return car as Car;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async getCars(page = 1, limit = 7) {
-    const response = await fetch(`${ApiService.link}/garage?_page=${page}&_limit=${limit}`, {
-      method: "GET",
-    });
-    if (response.ok) {
+    try {
+      const response = await fetch(`${ApiService.link}/garage?_page=${page}&_limit=${limit}`, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const cars = await response.json();
       return cars as Car[];
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async getWinners(page = 1, limit = 10, sort = "id", order = "ASC") {
-    const response = await fetch(
-      `${ApiService.link}/winners?_page=${page}&_limit=${limit}&_sort=${sort}&_order=${order}`,
-      {
-        method: "GET",
-      },
-    );
-
-    if (response.ok) {
+    try {
+      const response = await fetch(
+        `${ApiService.link}/winners?_page=${page}&_limit=${limit}&_sort=${sort}&_order=${order}`,
+        {
+          method: "GET",
+        },
+      );
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const winners = await response.json();
       return winners as Winner[];
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async getAllWinners() {
-    const response = await fetch(`${ApiService.link}/winners`, {
-      method: "GET",
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`${ApiService.link}/winners`, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const winners = await response.json();
       return winners as Winner[];
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async createWinner(data: Winner = {}) {
-    const response = await fetch(`${ApiService.link}/winners`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`${ApiService.link}/winners`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const car = await response.json();
       return car as Winner;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async updateWinner(id: number, data: Winner = {}) {
-    const response = await fetch(`${ApiService.link}/winners/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`${ApiService.link}/winners/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const car = await response.json();
       return car as Winner;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 
   static async getWinner(id: number) {
-    const response = await fetch(`${ApiService.link}/winners/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`${ApiService.link}/winners/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
+      }
       const car = await response.json();
       return car as Winner;
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error(`Something went wrong! ${response.status}: ${response.statusText}`);
   }
 }
 
